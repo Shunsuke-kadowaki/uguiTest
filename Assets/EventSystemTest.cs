101499f3ab015428c2467dbee7ec9a3080d4eadd
Shunsuke kadowaki <photolock4@gmail.com>
2014-09-15 23:51:28 +0900

    project commit

---
+++ Assets/EventSystemTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EventSystemTest : MonoBehaviour, IPointerClickHandler {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPointerClick (PointerEventData eventData) {
+
+		if(Time.timeScale <= 0) {
+			this._StartRotate();
+		} else {
+			this._StopRotate();
+		}
+
+	}
+
+	private void _StopRotate() {
+		Time.timeScale = 0;
+	}
+
+	private void _StartRotate() {
+		Time.timeScale = 1;
+	}
+}
---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+
+	public float speedX = 30.0f;
+	public float speedY = 40.0f;
+	public float speedZ = 50.0f;
+
+	public GameObject Cube;
+
+	private float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer = Time.deltaTime;
+
+		Cube.transform.Rotate(speedX * timer, speedY * timer, speedZ * timer);
+	}
+}
